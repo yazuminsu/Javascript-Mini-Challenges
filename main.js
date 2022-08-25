c6887458f92172b69c04514a91d3728dda51de7b
yazu <102346166+yazuminsu@users.noreply.github.com>
2022-08-25 11:07:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -125,3 +125,40 @@ describe("Fixed tests", () => {
     assert.strictEqual(countSheep(3), "1 sheep...2 sheep...3 sheep...");
   });
 });
+
+
+// Reverse words
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+//Answer:
+function reverseWords(str) {
+  const arr = str.split(' ');
+  let arrRev = [];
+  for(let a of arr){
+    arrRev.push((a.split('').reverse().join('')));
+  }
+  return(arrRev.join(' '));
+}
+
+//or:
+function reverseWords(str) {
+  return str.split("").reverse().join("").split(" ").reverse().join(" ");
+}
+
+//Test:
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(reverseWords('The quick brown fox jumps over the lazy dog.'), 'ehT kciuq nworb xof spmuj revo eht yzal .god');
+    assert.strictEqual(reverseWords('apple'), 'elppa');
+    assert.strictEqual(reverseWords('a b c d'), 'a b c d');
+    assert.strictEqual(reverseWords('double  spaced  words'), 'elbuod  decaps  sdrow');
+  });
+});
